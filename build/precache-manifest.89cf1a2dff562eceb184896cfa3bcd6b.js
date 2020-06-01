02784f78eb7d392e4966e1f841947fcd44ea2794
Shruti Priya <shruti@cronj.com>
2020-06-01 18:20:48 +0530

    created a build folder

---
+++ build/precache-manifest.89cf1a2dff562eceb184896cfa3bcd6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72519ddb9096b0f60d0e4dc4572942f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2ac03c0c1ff69c51838",
+    "url": "/static/css/main.078b61d3.chunk.css"
+  },
+  {
+    "revision": "f77c376eb43b8bcc7c08",
+    "url": "/static/js/2.fbe7caf9.chunk.js"
+  },
+  {
+    "revision": "493090b381bf13491e9f4fd623f40f56",
+    "url": "/static/js/2.fbe7caf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ac03c0c1ff69c51838",
+    "url": "/static/js/main.e5492ef8.chunk.js"
+  },
+  {
+    "revision": "29d1e3d0140d24da2528",
+    "url": "/static/js/runtime-main.270bc424.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
